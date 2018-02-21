f6c7a7af03b4209b382536dc0ac94f63dca1a0e6
aurorex <selva_beat@hotmail.com>
2018-02-21 06:03:01 +0100

    subiendo primeros estilos y agregando funcionalidad de la hora de publicación

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
-/* VERSIÓN 1 */
+// función para modal de materialize
+(function begin() {
+  $('.modal').modal();
+
+})();
+
 $(document).ready(function() {
+  // función para la hora
+  const time = () => {
+    let newDate = new Date();
+    let time = newDate.getHours() + ':' + newDate.getMinutes();
+    let timeAbsolute = '';
+ 
+    if (newDate.getHours() <= 12) {
+      timeAbsolute = time + ' AM';
+    } else {
+      timeAbsolute = time + ' PM';
+    }
+    return timeAbsolute;
+  };
 
 });
 
-
-/* VERSIÓN 2 */
-function begin() {
-
-}
-$(document).ready(begin);
